58e875cb8667d580c912595df533dad46c27f5f3
Brandon Kudo <brandonkudo@gmail.com>
2017-11-22 11:59:52 -0800

    add functions for mars, venus, and mercury years

--- js/calc.js
+++ js/calc.js
@@ -2,7 +2,20 @@ export class Years {
   constructor(age) {
     this.age = age;
   }
+
   yearsToSeconds(age) {
     return age * 31536000;
+  }
+
+  mercuryAge(age) {
+    return age * .24;
+  }
+
+  venusAge(age) {
+    return age * .62;
+  }
+
+  marsAge(age) {
+    return age * 1.88;
   }
 }
--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -2,7 +2,20 @@ import { Years } from './../js/calc.js';
 
 describe('Years', function() {
   var testAge = new Years(1)
+
   it('should return a persons age in seconds', function() {
     expect(testAge.yearsToSeconds(1)).toEqual(31536000);
+  });
+
+  it('should return a persons age in mars years', function() {
+    expect(testAge.mercuryAge(1)).toEqual(.24);
+  });
+
+  it('should return a persons age in venus years', function() {
+    expect(testAge.venusAge(1)).toEqual(.62);
+  });
+
+  it('should return a persons age in mars years', function() {
+    expect(testAge.marsAge(1)).toEqual(1.88);
   });
 });
