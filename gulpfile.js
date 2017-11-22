193c8582a121533e7d23b9f549b6574892fa1615
Brandon Kudo <brandonkudo@gmail.com>
2017-11-21 20:51:45 -0800

    hopefully install karma correctly

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require('del');
 var lib = require('bower-files')();
+var browserSync = require('browser-sync').create();
 var buildProduction = utilities.env.production;
 var jshint = require('gulp-jshint');
 
@@ -34,23 +35,47 @@ gulp.task("clean", function(){
   return del(['build', 'tmp']);
 });
 
-gulp.task("build", ['clean'], function(){
+gulp.task('bowerJS', function () {
+  return gulp.src(lib.ext('js').files)
+  .pipe(concat('vendor.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('bower', ['bowerJS']);
+
+gulp.task('build', ['clean'], function(){
   if (buildProduction) {
     gulp.start('minifyScripts');
   } else {
     gulp.start('jsBrowserify');
   }
-});
-
-gulp.task('bowerJS', function () {
-  return gulp.src(lib.ext('js').files)
-    .pipe(concat('vendor.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/js'));
+  gulp.start('bower');
 });
 
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+
 });
---
+++ spec/calculator-spec.js
@@ -0,0 +1,8 @@
+var Calculator = require('./../js/calculator.js').calculatorModule;
+
+describe('Calculator', function() {
+  it('should return the word ping if a 3', function() {
+    var testCalculator = new Calculator(1)
+    expect(testCalculator.)
+  });
+});
