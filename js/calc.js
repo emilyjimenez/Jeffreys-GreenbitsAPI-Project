89b12096a5d5bc6937c380ef9baeeddf7c0dba54
Brandon Kudo <brandonkudo@gmail.com>
2017-11-24 02:27:50 -0800

    fix age calculator function

--- js/calc.js
+++ js/calc.js
@@ -1,35 +1,36 @@
 export class Years {
-  constructor(age) {
-    this.age = age;
+  constructor(date) {
+    this.date = date;
   }
 
-  ageCalculator(age) {
-    const today = moment();
+
+  ageCalculator(date) {
+    const today = new Date();
     let todayInSeconds = today.getTime()/1000;
-    let birthdayInSeconds = birthday.getTime()/1000;
+    let birthdayInSeconds = date.getTime()/1000;
     let ageInSeconds = todayInSeconds - birthdayInSeconds;
-    let ageInYears = ageInSeconds * 31536000;
+    let ageInYears = ageInSeconds / 31536000;
     return ageInYears;
   }
 
-  yearsToSeconds(age) {
-    let secondsAge = (age * 31536000);
-    return secondsAge;
-  }
-
-  mercuryAge(age) {
-    return age * .24;
-  }
-
-  venusAge(age) {
-    return age * .62;
-  }
-
-  marsAge(age) {
-    return age * 1.88;
-  }
-
-  jupiterAge(age) {
-    return age * 11.86;
-  }
+  // yearsToSeconds(date) {
+  //   let secondsAge = this.ageCalculator(date);
+  //   return secondsAge;
+  // }
+  //
+  // mercuryAge(date) {
+  //   return age * .24;
+  // }
+  //
+  // venusAge(age) {
+  //   return age * .62;
+  // }
+  //
+  // marsAge(age) {
+  //   return age * 1.88;
+  // }
+  //
+  // jupiterAge(age) {
+  //   return age * 11.86;
+  // }
 }
--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -2,24 +2,29 @@ import { Years } from './../js/calc.js';
 
 describe('Years', function() {
   let testAge = new Years(1)
+  let testDate = new Date(2014, 11, 24);
 
-  it('should return a persons age in seconds', function() {
-    expect(testAge.yearsToSeconds(1)).toEqual(31536000);
+  it('should return a persons age', function() {
+    expect(testAge.ageCalculator(testDate)).toEqual(3)
   });
 
-  it('should return a persons age in mars years', function() {
-    expect(testAge.mercuryAge(1)).toEqual(.24);
-  });
-
-  it('should return a persons age in venus years', function() {
-    expect(testAge.venusAge(1)).toEqual(.62);
-  });
-
-  it('should return a persons age in mars years', function() {
-    expect(testAge.marsAge(1)).toEqual(1.88);
-  });
-
-  it('should return a persons age in jupiter years', function() {
-    expect(testAge.jupiterAge(1)).toEqual(11.86);
-  });
+  // it('should return a persons age in seconds', function() {
+  //   expect(testAge.yearsToSeconds(1)).toEqual(31536000);
+  // });
+  //
+  // it('should return a persons age in mars years', function() {
+  //   expect(testAge.mercuryAge(1)).toEqual(.24);
+  // });
+  //
+  // it('should return a persons age in venus years', function() {
+  //   expect(testAge.venusAge(1)).toEqual(.62);
+  // });
+  //
+  // it('should return a persons age in mars years', function() {
+  //   expect(testAge.marsAge(1)).toEqual(1.88);
+  // });
+  //
+  // it('should return a persons age in jupiter years', function() {
+  //   expect(testAge.jupiterAge(1)).toEqual(11.86);
+  // });
 });
