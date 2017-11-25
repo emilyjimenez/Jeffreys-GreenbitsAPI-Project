71e224e486426504c7290a1cfdf29cf92d2f942b
Brandon Kudo <brandonkudo@gmail.com>
2017-11-24 16:51:53 -0800

    finish mercury spec

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -3,19 +3,19 @@ import { Years } from './../js/calc.js';
 describe('Years', function() {
   const testAge = new Years();
   const testDate = new Date(2014, 11, 24);
-  let testAgeYears = testAge.ageCalculator(testDate) * 31536000;
+  let testAgeYears = testAge.ageCalculator(testDate);
 
   it('should return a persons age', function() {
     expect(Math.floor(testAge.ageCalculator(testDate))).toEqual(2);
   });
 
   it('should return a persons age in seconds', function() {
-    expect(Math.round(testAge.yearsToSeconds(testDate))).toEqual(Math.round(testAgeYears));
+    expect(Math.round(testAge.yearsToSeconds(testDate))).toEqual(Math.round(testAgeYears * 31536000));
   });
 
-  // it('should return a persons age in mars years', function() {
-  //   expect(testAge.mercuryAge(1)).toEqual(.24);
-  // });
+  it('should return a persons age in mars years', function() {
+    expect(Math.round(testAge.mercuryAge(testDate))).toEqual(Math.round(testAgeYears * .24));
+  });
   //
   // it('should return a persons age in venus years', function() {
   //   expect(testAge.venusAge(1)).toEqual(.62);
