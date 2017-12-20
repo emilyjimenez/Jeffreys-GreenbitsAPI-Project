b955f7d3d8498a4a47628bf3db1a5ed3b5f61755
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-12-20 10:06:26 -0800

    test interface display with callbacks.

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -1,7 +1,5 @@
 import { Lookup } from './../js/lookup.js';
 
-//Call back is not singling our flowers and is making me angry
-
 let displayFlowerData = function(response) {
   if (response.products.length > 0) {
     for (let i = 0; i < response.products.length; i++) {
@@ -12,7 +10,6 @@ let displayFlowerData = function(response) {
       let cbd = index.test_results_cbd;
       let price = index.sell_price;
       if (flowerType === 0) {
-        console.log("these are indica: " + name);
         $(".content").append(`<div class="list-item">
                                 <div class="types">
                                   <p class="icon-indica">I</p>
@@ -27,7 +24,6 @@ let displayFlowerData = function(response) {
 
         `);
       } else if (flowerType === 1) {
-        console.log("these are sativa: " + name);
         $(".content").append(`<div class="list-item">
                                 <div class="types">
                                   <p class="icon-sativa">S</p>
@@ -42,7 +38,6 @@ let displayFlowerData = function(response) {
 
         `);
       } else if (flowerType === 2) {
-        console.log("these are hybrid: " + name);
         $(".content").append(`<div class="list-item">
                                 <div class="types">
                                   <p class="icon-hybrid">H</p>
@@ -57,7 +52,6 @@ let displayFlowerData = function(response) {
 
         `);
       } else if (flowerType === 3) {
-        console.log("these are indica hybrid: " + name);
         $(".content").append(`<div class="list-item">
                                 <div class="types">
                                   <p class="icon-indica-hybrid">H</p>
@@ -72,7 +66,6 @@ let displayFlowerData = function(response) {
 
         `);
       } else {
-        console.log("these are sativa hybrid: " + name);
         $(".content").append(`<div class="list-item">
                                 <div class="types">
                                   <p class="icon-sativa-hybrid">H</p>
@@ -93,14 +86,63 @@ let displayFlowerData = function(response) {
 
 let displayPrerollData = function(response) {
   if (response.products.length > 0) {
-    console.log(response);
     for (let i = 0; i < response.products.length; i++) {
       let index = response.products[i];
       let name = index.name;
       let flowerType = index.flower_type;
       let thc = index.test_results_thc;
       let cbd = index.test_results_cbd;
-      console.log(name);
+    }
+  }
+};
+
+let displayCartridgeData = function(response) {
+  if (response.products.length > 0) {
+    $(".cartridge-content").append(`CARTRIDGE HEADER`);
+    for (let i = 0; i < response.products.length; i++) {
+      let index = response.products[i];
+      let name = index.name;
+      let flowerType = index.flower_type;
+      let concentrateType = index.concentrate_type;
+      let thc = index.test_results_thc;
+      let cbd = index.test_results_cbd;
+      let price = index.sell_price;
+      $(".cartridge-content").append(`STUFF`
+      );
+    }
+  }
+};
+
+let displayConcentrateData = function(response) {
+  if (response.products.length > 0) {
+    console.log(response);
+    $(".concentrate-content").append(`CONCENTRATE HEADER - WAX`);
+    $(".concentrate-content").append(`CONCENTRATE HEADER - SHATTER`);
+    $(".concentrate-content").append(`CONCENTRATE HEADER - LIVE RESIN`);
+    $(".concentrate-content").append(`CONCENTRATE HEADER - RSO`);
+    $(".concentrate-content").append(`CONCENTRATE HEADER - OIL`);
+    for (let i = 0; i < response.products.length; i++) {
+      let index = response.products[i];
+      let name = index.name;
+      let flowerType = index.flower_type;
+      let concentrateType = index.concentrate_type;
+      let thc = index.test_results_thc;
+      let cbd = index.test_results_cbd;
+      let price = index.sell_price;
+      //Only including concentrate types 0,1,2,8,and 9. When building out full version need to include all concentrate types
+      if (concentrateType === 0) {
+        $(".concentrate-content").append(`PLACE UNDER HEADER
+
+        `);
+      } else if (concentrateType === 1) {
+
+      } else if (concentrateType === 2) {
+
+      } else if (concentrateType === 8) {
+
+      } else {
+
+      }
     }
   }
 };
@@ -119,8 +161,20 @@ $(document).ready(function() {
     flowerLookup.getFlowerData(flowerID, displayFlowerData, error);
   });
   let prerollLookup = new Lookup();
-  let prerollID = "01ccfc60-ab96-4614-bfb8-8dea907e96f0"
+  let prerollID = "01ccfc60-ab96-4614-bfb8-8dea907e96f0";
   $("#check-preroll").click(function() {
+    $("#buttons").hide();
+    $("#preroll-menu").show();
     prerollLookup.getPrerollData(prerollID, displayPrerollData, error);
-  })
+  });
+  let cartridgeLookup = new Lookup();
+  let cartridgeID = "81135415-1a22-4c85-b332-d9eea906633d";
+  let concentrateLookup = new Lookup();
+  let concentrateID = "0ff11ccf-3221-46ae-ae81-9f81228b88cf";
+  $("#check-cartridge-concentrate").click(function() {
+    $("#buttons").hide();
+    $("#concentrate-menu").show();
+    cartridgeLookup.getCartridgeData(cartridgeID, displayCartridgeData, error);
+    concentrateLookup.getConcentrateData(concentrateID, displayConcentrateData, error);
+  });
 });
--- js/lookup.js
+++ js/lookup.js
@@ -45,4 +45,50 @@ export class Lookup {
       }
     });
   }
+
+
+//Limiting concentrate menu to only display 25 cartridges and 25 concentrates per notes from client on downsizing menu
+  getCartridgeData(response, displayCartridgeData, error) {
+    $.ajax({
+      //organized by vendor since that's what people want to scan when looking for a cartridge
+      url: `https://api.greenbits.com/api/v1/products?limit=25&by_active=true&by_product_type=${response}&sort_by[vendor]`,
+      headers: {
+        'Authorization': authorizationToken,
+      },
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+
+      displayCartridgeData(response);
+
+      },
+      error: function() {
+        alert('FAIL WHALE');
+      }
+    });
+  }
+
+  getConcentrateData(response, displayConcentrateData, error) {
+    $.ajax({
+      url: `https://api.greenbits.com/api/v1/products?limit=25&by_active=true&by_product_type=${response}`,
+      headers: {
+        'Authorization': authorizationToken,
+      },
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+
+      displayConcentrateData(response);
+
+      },
+      error: function() {
+        alert('FAIL WHALE');
+      }
+    });
+  }
+
 }
